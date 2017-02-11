2148bca489188a19302f31525ff21aac9ee8aa6c
Alex Patel <acpatel@andrew.cmu.edu>
2017-02-11 16:52:27 -0500

    Initial commit

---
+++ scripts/Growth.cs
@@ -0,0 +1,284 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Growth : MonoBehaviour {
+
+	static GameObject current_selected;
+
+    private int waterCount;
+    private float growthTimeLimit;
+    private bool growing;
+    private float growthFactor;
+    private float growthTime;
+    private bool plantable;
+    private float growthHeightFactor;
+
+    public GameObject plant;
+    public GameObject sunflower;
+    public GameObject cactus;
+    public GameObject rose;
+    public GameObject bonsai;
+    public GameObject grass;
+    public GameObject pot;
+    public GameObject dirtobject;
+    public GameObject sprinkleobject;
+
+    public Canvas canvas;
+
+    // Plant List
+    //
+    // Plant
+    // Sunflower
+    // Cactus
+    // Rose
+    // Bonsai Tree
+    // Grass
+    //
+
+    static Mesh plantModel;
+    static Mesh sunflowerModel;
+	static Mesh cactusModel;
+    static Mesh roseModel;
+    static Mesh bonsaiModel;
+    static Mesh grassModel;
+
+    static Material plantMaterial;
+    static Material sunflowerMaterial;
+    static Material cactusMaterial;
+    static Material roseMaterial;
+    static Material bonsaiMaterial;
+    static Material grassMaterial;
+
+    static float plantHeight;
+    static float sunflowerHeight;
+    static float cactusHeight;
+    static float roseHeight;
+    static float bonsaiHeight;
+    static float grassHeight;
+
+    static float plantScale;
+    static float sunflowerScale;
+    static float cactusScale;
+    static float roseScale;
+    static float bonsaiScale;
+    static float grassScale;
+
+    static float plantGrowth;
+    static float sunflowerGrowth;
+    static float cactusGrowth;
+    static float roseGrowth;
+    static float bonsaiGrowth;
+    static float grassGrowth;
+
+    private ParticleSystem dirt;
+    private ParticleSystem sprinkle;
+
+    public int buttonNumber;
+
+    // Use this for initialization
+    void Start () {
+
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        plantable = true;
+        waterCount = 0;
+        growing = false;
+        growthTimeLimit = 10.0f;
+        growthFactor = 0.2f;
+        growthHeightFactor = 0.1f;
+        growthTime = 0.0f;
+
+        plantModel = plant.GetComponent<MeshFilter>().mesh;
+        sunflowerModel = sunflower.GetComponent<MeshFilter>().mesh;	
+        cactusModel = cactus.GetComponent<MeshFilter>().mesh;
+        roseModel = rose.GetComponent<MeshFilter>().mesh;
+		bonsaiModel = bonsai.GetComponent<MeshFilter>().mesh;
+		grassModel = grass.GetComponent<MeshFilter>().mesh;
+
+        plantMaterial = plant.GetComponent<Renderer>().material;
+        sunflowerMaterial = sunflower.GetComponent<Renderer>().material;
+        cactusMaterial = cactus.GetComponent<Renderer>().material;
+        roseMaterial = rose.GetComponent<Renderer>().material;
+        bonsaiMaterial = bonsai.GetComponent<Renderer>().material;
+        grassMaterial = grass.GetComponent<Renderer>().material;
+
+        plantHeight = 0.01f;
+        sunflowerHeight = 0.13f;
+        cactusHeight = 0.01f;
+        roseHeight = -0.05f;
+        bonsaiHeight = 0.001f;
+        grassHeight = 0.005f;
+
+        plantScale = 0.04f;
+        sunflowerScale = 0.04f;
+        cactusScale = 0.04f;
+        roseScale = 0.001f;
+        bonsaiScale = 0.001f;
+        grassScale = 0.04f;
+
+        plantGrowth = 0.2f;
+        sunflowerGrowth = 0.2f;
+        cactusGrowth = 0.2f;
+        roseGrowth = 0.005f;
+        bonsaiGrowth = 0.03f;
+        grassGrowth = 0.2f;
+
+        dirt = dirtobject.GetComponent<ParticleSystem>();
+        sprinkle = sprinkleobject.GetComponent<ParticleSystem>();
+        dirt.Stop();
+        sprinkle.Stop();
+    }
+
+    void digUp ()
+    {
+        dirt.Play();
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        plantable = true;
+        growing = false;
+		waterCount = 0;
+    }
+
+    IEnumerator grow()
+    {
+        yield return new WaitForSeconds(1.7f);
+        growing = true;
+        yield return null;
+    }
+
+    void water () //Waters plant - initiates growth if not at Stage 3
+    {
+        sprinkle.Play();
+        if (waterCount < 2)
+        { 
+            waterCount++;
+            StartCoroutine("grow");
+        }
+    }
+
+    void clicked ()
+    {
+        if (buttonNumber == 0)
+        {
+            digUp();
+        }
+        if (gameObject.activeSelf && buttonNumber == 1 && growing == false)
+        {
+            water();
+        }
+		if (plantable && 2 <= buttonNumber)
+        {
+            switch (buttonNumber)
+            {
+                case 2:
+					setMeshElements(plantModel, plantMaterial);
+                    transform.localScale = new Vector3(plantScale, plantScale, plantScale);
+                    transform.localPosition = new Vector3(transform.localPosition.x, 0.1406f,transform.localPosition.z);
+                    growthHeightFactor = plantHeight;
+                    growthFactor = plantGrowth;
+                    break;
+                case 3:
+					setMeshElements(sunflowerModel, sunflowerMaterial);
+                    transform.localScale = new Vector3(sunflowerScale, sunflowerScale, sunflowerScale);
+                    transform.localPosition = new Vector3(transform.localPosition.x, 0.1406f, transform.localPosition.z);
+                    growthHeightFactor = sunflowerHeight;
+                    growthFactor = sunflowerGrowth;                   
+                    break;
+                case 4:
+					setMeshElements(cactusModel, cactusMaterial);
+                    transform.localScale = new Vector3(cactusScale, cactusScale, cactusScale);
+                    transform.localPosition = new Vector3(transform.localPosition.x, 0.1406f, transform.localPosition.z);
+                    growthHeightFactor = cactusHeight;
+                    growthFactor = cactusGrowth;
+                    break;
+                case 5:
+					setMeshElements(roseModel, roseMaterial);
+                    transform.localScale = new Vector3(roseScale, roseScale, roseScale);
+                    transform.localPosition = new Vector3(transform.localPosition.x, 0.1406f, transform.localPosition.z);
+                    growthHeightFactor = roseHeight;
+                    growthFactor = roseGrowth;
+                    break;
+                case 6:
+					setMeshElements(bonsaiModel, bonsaiMaterial);
+                    transform.localScale = new Vector3(bonsaiScale, bonsaiScale, bonsaiScale);
+                    transform.localPosition = new Vector3(transform.localPosition.x, 0.1406f, transform.localPosition.z);
+                    growthHeightFactor = bonsaiHeight;
+                    growthFactor = bonsaiGrowth;
+                    break;
+                case 7:
+					setMeshElements(grassModel, grassMaterial);
+                    transform.localScale = new Vector3(grassScale, grassScale, grassScale);
+                    transform.localPosition = new Vector3(transform.localPosition.x, 0.1406f, transform.localPosition.z);
+                    growthHeightFactor = grassHeight;
+                    growthFactor = grassGrowth;
+                    break;
+            }
+			gameObject.GetComponent<MeshRenderer> ().enabled = true;
+            plantable = false;
+			growing = false;
+        }
+    }
+
+	void setMeshElements(Mesh mesh, Material material) {
+		gameObject.GetComponent<MeshFilter> ().mesh = mesh;
+		gameObject.GetComponent<MeshCollider> ().sharedMesh = mesh;
+        gameObject.GetComponent<Renderer>().material = material;
+	}
+
+	void getTouch() {
+		if (Input.GetMouseButtonDown(0))
+			//Input.GetMouseButtonDown(0)
+			//Input.touchCount > 0
+		{
+            /*
+            if (EventSystem.current.IsPointerOverGameObject (Input.GetTouch (0).fingerId)) {
+				//UIPlantControl.data_text.text = "cancelling";
+				return;
+			}*/
+
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			//Input.mousePosition
+			//Input.GetTouch(0).position
+			if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+			{
+				UIPlantControl.data_text.text = hit.collider.gameObject.name;
+				if (hit.collider.gameObject == gameObject || hit.collider.gameObject == pot) {
+                    if (UIPlantControl.info_mode)
+                    {
+                        if (hit.collider.gameObject == gameObject) { 
+                        canvas.GetComponent<UIPlantControl>().activateMoreInfoPanel(gameObject.GetComponent<MeshFilter>().mesh.name);
+                        }
+                    } else {
+                        current_selected = gameObject;
+                        buttonNumber = UIPlantControl.plant_chosen;
+                        UIPlantControl.data_text.text = buttonNumber.ToString();
+                        clicked();
+                    }
+				}
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+    {
+		getTouch ();
+
+        if (growing)
+        {
+            float dT = Time.deltaTime / growthTimeLimit;
+            Vector3 v1 = new Vector3(growthFactor * dT / 1.3f, growthFactor * dT, growthFactor * dT / 1.3f);
+            transform.localScale += v1;
+            Vector3 v2 = new Vector3(0,growthHeightFactor * dT,0);
+            transform.localPosition += v2;
+
+            growthTime += Time.deltaTime;
+            if (growthTime >= growthTimeLimit)
+            {
+                growing = false;
+                growthTime = 0.0f;
+            }
+        }
+	}
+}
---
+++ scripts/UIPlantControl.cs
@@ -0,0 +1,300 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.EventSystems;
+
+public class UIPlantControl : MonoBehaviour
+{
+
+    public static int plant_chosen;
+    public static bool info_mode;
+
+    public Button plant_button, sunflower_button, cactus_button, rose_button, bonsai_button, grass_button, water_button, dig_button, more_info_button;
+    public Button exit_button;
+    public Image button_bar, more_info_panel;
+    public static Text data_text;
+
+    public static float scale = 1.5f;
+    public static float down = scale * -90f;
+
+    public Sprite plantSprite, sunflowerSprite, cactusSprite, roseSprite, bonsaiSprite, grassSprite;
+
+    List<Button> list_of_plant_buttons;
+    List<Button> list_of_visible;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        data_text = gameObject.transform.GetChild(gameObject.transform.childCount - 1).GetComponent<Text>();
+
+        list_of_plant_buttons = new List<Button>();
+        list_of_visible = new List<Button>();
+
+        list_of_plant_buttons.Add(plant_button);
+        list_of_plant_buttons.Add(sunflower_button);
+        list_of_plant_buttons.Add(cactus_button);
+        list_of_plant_buttons.Add(rose_button);
+        list_of_plant_buttons.Add(bonsai_button);
+        list_of_plant_buttons.Add(grass_button);
+
+
+        //setting positions of the buttons
+        for (int i = 0; i < list_of_plant_buttons.Count; i++)
+        {
+            if (i < 3)
+            {
+                list_of_plant_buttons[i].gameObject.SetActive(true);
+                list_of_plant_buttons[i].GetComponent<RectTransform>().anchoredPosition = new Vector3(140 * scale * (i - 1), down, 0);
+                list_of_visible.Add(list_of_plant_buttons[i]);
+            }
+            else
+            {
+                list_of_plant_buttons[i].gameObject.SetActive(false);
+            }
+
+            //changes the scale of the objects
+            list_of_plant_buttons[i].transform.localScale = new Vector3(scale, scale, scale);
+        }
+
+        dig_button.transform.localScale = new Vector3(scale, scale, scale);
+        water_button.transform.localScale = new Vector3(scale, scale, scale);
+        dig_button.GetComponent<RectTransform>().anchoredPosition = new Vector3(140 * scale * -2, down, 0);
+        water_button.GetComponent<RectTransform>().anchoredPosition = new Vector3(140 * scale * 2, down, 0);
+
+        button_bar.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, down, 0);
+        button_bar.transform.localScale = new Vector3(scale, scale, scale);
+
+        exit_button.GetComponent<RectTransform>().anchoredPosition = new Vector3(-down, down, 0);
+        exit_button.transform.localScale = new Vector3(scale, scale, scale);
+        exit_button.gameObject.SetActive(false);
+
+        more_info_button.GetComponent<RectTransform>().anchoredPosition = new Vector3(down, down, 0);
+        more_info_button.transform.localScale = new Vector3(scale, scale, scale);
+
+        more_info_panel.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -60 * scale, 0);
+        more_info_panel.transform.localScale = new Vector3(scale, scale, scale);
+        more_info_panel.gameObject.SetActive(false);
+    }
+
+    void getTouches()
+    {
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            scroll(true, 5);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            scroll(false, 5);
+        }
+#endif
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+
+            if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            {
+                return;
+            }
+
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            float touchTime = Input.GetTouch(0).deltaTime;
+            float touchSpeed = touchDeltaPosition.x / touchTime;
+
+            if (Math.Abs(touchSpeed) > 2000)
+            {
+                if (touchSpeed < 0)
+                {
+                    UIPlantControl.data_text.text = "v: " + touchSpeed.ToString();
+                    scroll(false, Math.Min(20, Math.Abs(touchSpeed / 800)));
+                }
+                else
+                {
+                    UIPlantControl.data_text.text = "v: " + touchSpeed.ToString();
+                    scroll(true, Math.Min(20, Math.Abs(touchSpeed / 800)));
+                }
+            }
+        }
+    }
+
+    void scroll(bool right, float mag)
+    {
+
+        if (right)
+        {
+            foreach (Button plant in list_of_visible)
+            {
+                float prev_x = plant.GetComponent<RectTransform>().anchoredPosition.x;
+                float new_x = prev_x + 10 * mag;
+
+                plant.GetComponent<RectTransform>().anchoredPosition = new Vector3(new_x, down, 0);
+            }
+
+            Button plant_ = list_of_visible[0];
+
+            if (plant_ == list_of_plant_buttons[0])
+            {
+                if (plant_.GetComponent<RectTransform>().anchoredPosition.x > -140 * scale)
+                {
+
+                    list_of_visible[0].GetComponent<RectTransform>().anchoredPosition = new Vector3(-140 * scale, down, 0);
+                    list_of_visible[1].GetComponent<RectTransform>().anchoredPosition = new Vector3(0, down, 0);
+                    list_of_visible[2].GetComponent<RectTransform>().anchoredPosition = new Vector3(140 * scale, down, 0);
+
+                    if (list_of_visible.Count == 4)
+                    {
+                        list_of_visible[3].gameObject.SetActive(false);
+                        list_of_visible.RemoveAt(3);
+                    }
+                }
+                return;
+            }
+
+            plant_ = list_of_visible[list_of_visible.Count - 1];
+            int index = list_of_plant_buttons.IndexOf(list_of_visible[0]);
+            float x_of_prev = list_of_visible[0].GetComponent<RectTransform>().anchoredPosition.x;
+
+            if (plant_.GetComponent<RectTransform>().anchoredPosition.x > 140 * scale)
+            {
+                if (list_of_visible.Count < 4)
+                {
+                    Button new_plant = list_of_plant_buttons[index - 1];
+                    new_plant.GetComponent<RectTransform>().anchoredPosition = new Vector3(x_of_prev - 140 * scale, down, 0);
+                    new_plant.gameObject.SetActive(true);
+                    list_of_visible.Insert(0, new_plant);
+                }
+                if (plant_.GetComponent<RectTransform>().anchoredPosition.x >= 280 * scale)
+                {
+                    list_of_visible.Remove(plant_);
+                    plant_.gameObject.SetActive(false);
+                }
+            }
+
+        }
+        else
+        {
+            foreach (Button plant in list_of_visible)
+            {
+                float prev_x = plant.GetComponent<RectTransform>().anchoredPosition.x;
+                float new_x = prev_x - 10 * mag;
+
+                plant.GetComponent<RectTransform>().anchoredPosition = new Vector3(new_x, down, 0);
+            }
+
+            Button plant_ = list_of_visible[list_of_visible.Count - 1];
+
+            if (plant_ == list_of_plant_buttons[list_of_plant_buttons.Count - 1])
+            {
+                if (plant_.GetComponent<RectTransform>().anchoredPosition.x < 140 * scale)
+                {
+
+                    int adder = 0;
+
+                    if (list_of_visible.Count > 3)
+                    {
+                        adder = 1;
+                    }
+                    list_of_visible[adder].GetComponent<RectTransform>().anchoredPosition = new Vector3(-140 * scale, down, 0);
+                    list_of_visible[1 + adder].GetComponent<RectTransform>().anchoredPosition = new Vector3(0, down, 0);
+                    list_of_visible[2 + adder].GetComponent<RectTransform>().anchoredPosition = new Vector3(140 * scale, down, 0);
+
+                    if (list_of_visible.Count == 4)
+                    {
+                        list_of_visible[0].gameObject.SetActive(false);
+                        list_of_visible.RemoveAt(0);
+                    }
+                }
+                return;
+            }
+
+            plant_ = list_of_visible[0];
+            int index = list_of_plant_buttons.IndexOf(list_of_visible[list_of_visible.Count - 1]);
+            float x_of_prev = list_of_visible[list_of_visible.Count - 1].GetComponent<RectTransform>().anchoredPosition.x;
+
+            if (plant_.GetComponent<RectTransform>().anchoredPosition.x < -140 * scale)
+            {
+                if (list_of_visible.Count < 4)
+                {
+                    Button new_plant = list_of_plant_buttons[index + 1];
+                    new_plant.GetComponent<RectTransform>().anchoredPosition = new Vector3(x_of_prev + 140 * scale, down, 0);
+                    new_plant.gameObject.SetActive(true);
+                    list_of_visible.Add(new_plant);
+                }
+                if (plant_.GetComponent<RectTransform>().anchoredPosition.x <= -280 * scale)
+                {
+                    list_of_visible.Remove(plant_);
+                    plant_.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void on_info_clicked()
+    {
+        UIPlantControl.info_mode = true;
+        exit_button.gameObject.SetActive(true);
+    }
+
+    public void activateMoreInfoPanel(string name)
+    {
+        
+        switch (name.Substring(0, 3))
+        {
+            case "pla":
+                more_info_panel.sprite = plantSprite;
+                break;
+            case "obj":
+                more_info_panel.sprite = sunflowerSprite;
+                break;
+            case "Cac":
+                more_info_panel.sprite = cactusSprite;
+                break;
+            case "F_f":
+                more_info_panel.sprite = roseSprite;
+                break;
+            case "Mes":
+                more_info_panel.sprite = bonsaiSprite;
+                break;
+            case "gra":
+                more_info_panel.sprite = grassSprite;
+                break;
+        }
+
+        more_info_panel.gameObject.SetActive(true);
+    }
+
+    public void on_exit_clicked()
+    {
+        UIPlantControl.info_mode = false;
+        exit_button.gameObject.SetActive(false);
+        more_info_panel.gameObject.SetActive(false);
+    }
+
+    public void plant_button_clicked()
+    {
+
+        if (EventSystem.current.currentSelectedGameObject.GetComponent<Button>() == dig_button)
+        {
+            plant_chosen = 0;
+        }
+        else if (EventSystem.current.currentSelectedGameObject.GetComponent<Button>() == water_button)
+        {
+            plant_chosen = 1;
+        }
+        else
+        {
+            plant_chosen = list_of_plant_buttons.IndexOf(EventSystem.current.currentSelectedGameObject.GetComponent<Button>()) + 2;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        getTouches();
+    }
+}
